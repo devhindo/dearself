66c439060f3b7a12219da63b9ae1a26d6acc77be
devhindo <lildevhind@gmail.com>
2023-12-06 18:58:03 +0200

    added ping pong

--- server/main.go
+++ server/main.go
@@ -3,12 +3,30 @@ package main
 import (
 	"github.com/devhindo/dearself/server/config"
 	"github.com/devhindo/dearself/server/api"
+
+	"fmt"
+	"net/http"
+	"time"
 )
 
 // todo make it a gin framework
 // todo also use the todo extension
 
 func main() {
+
+	go func() {
+        for {
+            _, err := http.Get("https://dearself.onrender.com/ping")
+            if err != nil {
+                // Handle error
+                fmt.Println(err)
+            }
+            // Wait for 14 minutes before sending the next request
+            time.Sleep(14 * time.Minute)
+			fmt.Println("pinged")
+        }
+    }()
+
 	config.LoadEnv()
 	api.RUN()
 }
