e272e89dfb5ce5ec71ad74377fbabc3334fc17b5
devhindo <lildevhind@gmail.com>
2024-03-21 05:27:21 +0200

    Refactor main.go file

--- server/main.go
+++ server/main.go
@@ -5,43 +5,36 @@ import (
 	"net/http"
 	"time"
 
-	"github.com/devhindo/dearself/server/config"
 	"github.com/devhindo/dearself/server/api"
-    "github.com/devhindo/dearself/server/db"
+	"github.com/devhindo/dearself/server/config"
+	"github.com/devhindo/dearself/server/db"
 )
 
 func main() {
 
-    config.LoadEnv()
-    db.RunScheduledMailsJob()
-
-    
+	config.LoadEnv()
+	db.RunScheduledMailsJob()
 
 	go func() {
-        for {
-            _, err := http.Get("https://dearself.onrender.com/ping")
-            if err != nil {
-                // Handle error
-                fmt.Println(err)
-            }
-            // Wait for 14 minutes before sending the next request
-            time.Sleep(14 * time.Minute)
+		for {
+			_, err := http.Get("https://dearself.onrender.com/ping")
+			if err != nil {
+				// Handle error
+				fmt.Println(err)
+			}
+			// Wait for 14 minutes before sending the next request
+			time.Sleep(14 * time.Minute)
 			fmt.Println("pinged")
-        }
-    }()
+		}
+	}()
 
-    go func() {
-        for {
-            db.RunScheduledMailsJob()
-            // send scheduled mails every 24 hours
-            time.Sleep(24 * time.Hour)
-        }
-    }()
+	go func() {
+		for {
+			db.RunScheduledMailsJob()
+			// send scheduled mails every 24 hours
+			time.Sleep(24 * time.Hour)
+		}
+	}()
 
 	api.RUN()
 }
-
-
-
-
-
